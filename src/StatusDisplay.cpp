60ed2bdab209abb95b84427798da357f78f67a81
harpua555 <ztbenoit@gmail.com>
2026-01-01 09:32:53 -0500

    Leave debug intact, revert status to base calc

--- src/StatusDisplay.cpp
+++ src/StatusDisplay.cpp
@@ -331,11 +331,12 @@ static void drawStatus(DisplayStatus status)
             // Line 2: Friendly format (X.XX weeks/days/hours/mins)
             // Line 3: Raw seconds
             {
+                /*
                 // DEBUG: Add offset to test different uptime values
                 const unsigned long DEBUG_UPTIME_OFFSET_SEC = 621132;
                 unsigned long uptimeSec = millis() / 1000 + DEBUG_UPTIME_OFFSET_SEC;
-
-                //unsigned long uptimeSec = millis() / 1000;
+                */
+                unsigned long uptimeSec = millis() / 1000;
 
                 display.setTextSize(1);
 
